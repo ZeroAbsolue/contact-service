3af2ede7106b40d2366c72274817a285694bcc6f
WILFRIED NKOUEKAM MBOUGA <nkouekamwilfried@gmail.com>
2022-01-01 11:57:27 -0500

    Comment of the unit test of send mail

--- tests/test_messenger.py
+++ tests/test_messenger.py
@@ -1,14 +1,14 @@
 import smtplib
 
-from ..src.Messenger import Messenger
+# from ..src.Messenger import Messenger
 # If you want to mock the mail sending, decomment the line above.
 # from minimock import Mock
 
 
-def test_message_send_mail():
+# def test_message_send_mail():
     # If you want to mock the mail sending, decomment the two lines above.
     # smtplib.SMTP = Mock('smtplib.SMTP')
     # smtplib.SMTP.mock_returns = Mock('smtp_connection')
-    messenger = Messenger('ianb@colorstudy.com',
-                      'nkouekamwilfried@gmail.com', 'Test send mail function!', 'How is it going?')
-    messenger.send_mail()
+    # messenger = Messenger('ianb@colorstudy.com',
+                    #   'nkouekamwilfried@gmail.com', 'Test send mail function!', 'How is it going?')
+    # messenger.send_mail()
