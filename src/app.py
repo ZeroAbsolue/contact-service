c0c33c541445a1750b67993d49d7d9cf37cee8a6
WILFRIED NKOUEKAM MBOUGA <nkouekamwilfried@gmail.com>
2022-01-02 18:27:07 -0500

    Set * to allow_origins

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ import falcon
 from .MessengerRessource import MessengerRessource
 
 app=application =  falcon.App(middleware=falcon.CORSMiddleware(
-    allow_origins='colisxchange.com'))
+    allow_origins='*'))
 sended_mail = MessengerRessource()
 
 app.add_route('/api/send_mail',sended_mail)
