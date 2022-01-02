b68664fb679fedd9f3623cdf29f95f04d22c30ee
WILFRIED NKOUEKAM MBOUGA <nkouekamwilfried@gmail.com>
2022-01-02 18:24:49 -0500

    Mise a jour cors settings

--- src/app.py
+++ src/app.py
@@ -2,7 +2,8 @@ import falcon
 
 from .MessengerRessource import MessengerRessource
 
-app=application =  falcon.App()
+app=application =  falcon.App(middleware=falcon.CORSMiddleware(
+    allow_origins='colisxchange.com'))
 sended_mail = MessengerRessource()
 
 app.add_route('/api/send_mail',sended_mail)
